01917d039032874821c426006a8a76b45fc58e58
Mike Covington <mfcovington@gmail.com>
2015-04-22 23:51:08 -0700

    Register BrowserAdmin

--- cms_genome_browser/admin.py
+++ cms_genome_browser/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from cms_genome_browser.models import Browser
 
-# Register your models here.
+class BrowserAdmin(admin.ModelAdmin):
+
+    fieldset_browser = ('Genome browser', {
+        'fields': [
+            'name',
+            'coordinate_system',
+        ],
+    })
+
+    fieldset_default_range = ('Default Range', {
+        'fields': [
+            'chr',
+            'start',
+            'end',
+        ],
+    })
+
+    fieldset_advanced = ('Advanced', {
+        'fields': [
+            'slug',
+        ],
+        'classes': [
+            'collapse',
+        ],
+    })
+
+    fieldsets = [
+        fieldset_browser,
+        fieldset_default_range,
+        fieldset_advanced,
+    ]
+
+    list_display = (
+        'name',
+        'coordinate_system',
+    )
+    list_filter = (
+        'coordinate_system__species__name',
+        'coordinate_system__auth',
+        'coordinate_system__ucsc_name',
+    )
+    search_fields = (
+        'name',
+        'coordinate_system__species__name',
+        'coordinate_system__species__taxid',
+        'coordinate_system__auth',
+        'coordinate_system__version',
+        'coordinate_system__ucsc_name',
+    )
+
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Browser, BrowserAdmin)
