3f83359e5faefc8e7f4008023a84a6408f5d377f
Mike Covington <mfcovington@gmail.com>
2015-04-25 19:32:54 -0700

    Create Browser list and detail views

--- cms_genome_browser/views.py
+++ cms_genome_browser/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, ListView
+from cms_genome_browser.models import Browser
 
-# Create your views here.
+class BrowserListView(ListView):
+    model = Browser
+    template_name = 'cms_genome_browser/browser_list.html'
+    context_object_name = 'browser_list'
+
+
+class BrowserDetailView(DetailView):
+    model = Browser
+    template_name = 'cms_genome_browser/browser_detail.html'
+    context_object_name = 'browser'
