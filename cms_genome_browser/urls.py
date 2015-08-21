7fc5d07932587c87e8484190c84b6936a2b7247e
Mike Covington <mfcovington@gmail.com>
2015-08-21 15:57:17 -0700

    Append trailing slash to detail URL

--- cms_genome_browser/urls.py
+++ cms_genome_browser/urls.py
@@ -3,5 +3,5 @@ from cms_genome_browser.views import BrowserListView, BrowserDetailView
 
 urlpatterns = patterns('',
     url(r'^$', BrowserListView.as_view(), name='browser_list'),
-    url(r'^(?P<slug>[^/]+)$', BrowserDetailView.as_view(), name='browser_detail'),
+    url(r'^(?P<slug>[^/]+)/$', BrowserDetailView.as_view(), name='browser_detail'),
 )
